c6937dc1333dfd20d060f4f6e6c239c10feeab02
Bruno Preto <brunommpreto@gmail.com>
2023-02-19 10:05:44 +0000

    Added helpers.min.js

--- Defaults/helpers.js
+++ Defaults/helpers.js
@@ -1,4 +1,4 @@
-import { config } from './config.js';
+import { config } from 'https://rawcdn.githack.com/Tribalwars-Scripts/Essentials/91d921398078f35867247a44d896e1b2ce6ef525/Defaults/config.js';
 
 const DEF_DELAY = config.DEF_DELAY || 1000;
 function sleep(ms) {
@@ -7,3 +7,5 @@ function sleep(ms) {
 function getMilliseconds(hrs, min, sec) {
 	return ((hrs * 60 * 60 + min * 60 + sec) * 1000);
 }
+
+export { sleep, getMilliseconds };
